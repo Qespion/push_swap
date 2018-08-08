e5c02387a534bd94bc1dc872f3cfc2b99d1752af
Quint88 <q.espion@gmail.com>
2018-08-08 19:27:57 +0200

    visu

--- visu.py
+++ visu.py
@@ -1,52 +1,142 @@
-# **************************************************************************** #
+# # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
 #    visu.py                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: oespion <oespion@student.42.fr>            +#+  +:+       +#+         #
+#    By: avo <avo@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/08/05 14:57:28 by oespion           #+#    #+#              #
-#    Updated: 2018/08/05 17:42:22 by oespion          ###   ########.fr        #
+#    Updated: 2018/08/06 16:08:11 by avo              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
-from tkinter import Tk, Label, Button
+import tkinter
+import time
 
 class MyFirstGUI:
-    def __init__(self, master):
+    def __init__(self, master, mov, ret):
         self.master = master
+        self.mov = mov
+        self.i = 0
+        self.master.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+        self.ret = ret.split()
+        self.ret1 = ['']
+        self.oP = tkinter.StringVar()
+        self.vA = tkinter.StringVar()
+        self.vB = tkinter.StringVar()
+        self.oP.set("")
+        self.vA.set("list A = " + ret)
+        self.vB.set("list B = ")
         master.title("Push Swap Visualisation")
 
-        self.lista = Label(master, text="list a = " + ret)
+        self.op = tkinter.Label(master, textvariable=self.oP)
+        self.op.pack()
+
+        self.lista = tkinter.Label(master, textvariable=self.vA, wraplengt=1100)
         self.lista.pack()
 
-        if (ret1):
-            self.listb = Label(master, text="list b = " + ret1)
-            self.listb.pack()
-        else:
-            self.listb = Label(master, text="list b is Empty")
-            self.listb.pack()
+        self.delim = tkinter.Label(master, text="-----------------------------")
+        self.delim.pack()
 
-        self.prev_button = Button(master, text="Prev", command=self.prev)
+        self.listb = tkinter.Label(master, textvariable=self.vB, wraplengt=1100)
+        self.listb.pack()
+        
+        self.prev_button = tkinter.Button(master, text="Prev", command=self.prev)
         self.prev_button.pack()
 
-        self.next_button = Button(master, text="Next", command=self.next)
+        self.next_button = tkinter.Button(master, text="Next", command=self.next)
         self.next_button.pack()
 
-        self.close_button = Button(master, text="Quit", command=master.quit)
+        self.tri_button = tkinter.Button(master, text="tri auto", command=self.tri)
+        self.tri_button.pack()
+
+        self.reset_button = tkinter.Button(master, text="reset", command=self.reset)
+        self.reset_button.pack()
+
+        self.close_button = tkinter.Button(master, text="Quit", command=master.quit)
         self.close_button.pack()
 
+    def update(self):
+        self.vA.set("list A = " + ' '.join(self.ret))
+        self.vB.set("list B = " + ' '.join(self.ret1))
+        self.oP.set(self.mov[self.i])
+
+    def handle(self, action):
+        if (action == "pb"):
+            if (self.ret1 == ['']):
+                self.ret1.insert(0, self.ret[0])                
+                self.ret1.pop()
+            else:
+                self.ret1.insert(0, self.ret[0])
+            self.ret = self.ret[1:]
+        elif (action == "ra"):
+            self.ret.append(self.ret[0])
+            self.ret = self.ret[1:]
+        elif (action == "rb"):
+            self.ret1.append(self.ret1[0])
+            self.ret1 = self.ret1[1:]
+        elif (action == "pa"):
+            self.ret.insert(0, self.ret1[0])
+            self.ret1 = self.ret1[1:]
+        elif (action == "sa"):
+            self.ret[0], self.ret[1] = self.ret[1], self.ret[0]
+        elif (action == "sb"):
+            self.ret1[0], self.ret1[1] = self.ret1[1], self.ret1[0]
+        elif (action == "ss"):
+            self.ret[0], self.ret[1] = self.ret[1], self.ret[0]
+            self.ret1[0], self.ret1[1] = self.ret1[1], self.ret1[0]
+        elif (action == "rr"):
+            self.ret.append(self.ret[0])
+            self.ret = self.ret[1:]
+            self.ret1.append(self.ret1[0])
+            self.ret1 = self.ret1[1:]
+        elif (action == "rra"):
+            self.ret.insert(0, self.ret[len(self.ret) - 1])
+            self.ret.pop()
+        elif (action == "rrb"):
+            self.ret1.insert(0, self.ret1[len(self.ret1) - 1])
+            self.ret1.pop()
+        elif (action == "rrr"):
+            self.ret1.insert(0, self.ret1[len(self.ret1) - 1])
+            self.ret1.pop()
+            self.ret.insert(0, self.ret[len(self.ret) - 1])
+            self.ret.pop()
+        self.update()
+
     def prev(self):
-        print("go prev!")
+        print(self.mov[self.i])
+        self.handle(mov[self.i])  
+        if self.i == 0: 
+            self.i = self.i
+        else:
+            self.i -= 1
 
     def next(self):
-        print("go next!")
+        self.handle(self.mov[self.i])
+        print(self.mov[self.i])
+        if self.i == len(mov) - 1:
+            self.next_button.config(state = "disabled")
+        else:
+            self.i += 1
 
-root = Tk()
+    def tri(self):
+        self.handle(mov[self.i])
+        self.i += 1
+        if self.i < len(mov):
+            self.master.after(int(50), self.tri)
+        if self.i == len(mov):
+            self.ret1 == ['']
+    
+    def reset(self):
+        self.ret = ret.split()
+        self.ret1 = ['']
+        self.i = 0
+        self.update()
+
+root = tkinter.Tk()
 with open(sys.argv[1], 'r') as my_file:
     ret = my_file.readline()
-    mov = my_file.read()
-ret1 = None
-my_gui = MyFirstGUI(root)
+    mov = my_file.read().split()
+my_gui = MyFirstGUI(root, mov, ret)
 root.mainloop()
